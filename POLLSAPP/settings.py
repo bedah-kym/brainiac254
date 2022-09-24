1ed889f998e6c94f450eab686db80c58b0242f2c
BEDAH <>
2022-09-24 10:27:52 +0300

    fixed login ui

--- POLLSAPP/settings.py
+++ POLLSAPP/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls',
-    'USERS'
+    'USERS',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -122,7 +123,7 @@ STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = 'polls:home'
 LOGOUT_REDIRECT_URL = 'polls:home'
 
-
+CRISPY_TEMPLATE_PACK =bootstrap4
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
