fa65b9b003488e50ffffa260fad119473c81ca04
BEDAH <>
2022-09-24 10:30:43 +0300

    fixed bottstrap

--- POLLSAPP/settings.py
+++ POLLSAPP/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = 'polls:home'
 LOGOUT_REDIRECT_URL = 'polls:home'
 
-CRISPY_TEMPLATE_PACK =bootstrap4
+CRISPY_TEMPLATE_PACK ='bootstrap4'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
