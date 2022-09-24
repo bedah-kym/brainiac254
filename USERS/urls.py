059a8605d7a9b2acae6bfa019d84edd1a9c92263
BEDAH <>
2022-09-24 10:13:12 +0300

    fixed login bug

--- USERS/urls.py
+++ USERS/urls.py
@@ -2,10 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.auth import views as vw
 
 app_name='USERS'
 urlpatterns = [
     path('register/',views.register,name="user-register"),
-    path('login/',views.login,name="user-login")
+    path('login/',vw.LoginView.as_view(template_name='USERS/login.html'),name='login'),
+    path('logout/',vw.LogoutView.as_view(template_name='polls/index.html'),name='logout'),
 
 ]
