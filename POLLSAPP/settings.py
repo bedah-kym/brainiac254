cd0c310f50cae3137c8f8e697305df035282a0bc
BEDAH <>
2022-09-24 10:01:26 +0300

    login redirect

--- POLLSAPP/settings.py
+++ POLLSAPP/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -119,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
+LOGIN_REDIRECT_URL = 'polls:home'
+LOGOUT_REDIRECT_URL = 'polls:home'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
