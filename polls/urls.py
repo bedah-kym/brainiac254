3ef8d17adf546d15ad3b0e6ea2216bbf608b30c7
BEDAH <>
2022-09-24 13:42:55 +0300

    login form try 2

--- polls/urls.py
+++ polls/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name='polls'
 
 urlpatterns = [
-    path('home/',views.homeview.as_view(),name="home"),
+    path('',views.homeview.as_view(),name="home"),
     path('result/<int:pk>/',views.result.as_view(),name="result"),
     path('delete/<int:pk>/',views.deletenewpoll.as_view(),name="delete"),
     path('details/<int:question_id>/',views.details,name="detail"),
